3eca2f949c4632a7b5f2ed878c6001bfb9a2c86b
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-25 12:17:36 +0200

    adding dynamic menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+     <v-app-bar app>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">CONTACTBookApp</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-for="item in dynamicMenu" :key="item.title" :to="item.link" text>
+        <v-icon left dark>{{item.icon}}</v-icon>
+        <span class="mr-2">{{item.title}}</span>
+      </v-btn>
+    </v-app-bar>
     <main>
       <router-view></router-view>
     </main>
@@ -9,7 +19,12 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      dynamicMenu:[
+        {icon: 'mdi-home', title: 'HOME', link: '/'},
+        {icon: 'mdi-account-plus',title: 'ADD CONTACT', link: '/add-contact'} 
+      ]
+    };
   }
 };
 </script>
